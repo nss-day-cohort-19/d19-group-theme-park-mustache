108201ba8b90988399c627da358e80f44ab0e8da
Azim <azimsadikov@gmail.com>
2017-05-16 11:09:49 -0500

    added links its pulling the data

--- js/requests.js
+++ js/requests.js
@@ -13,69 +13,74 @@ let map = {};
 map.loadAreas = function() {
   return new Promise((resolve, reject) => {
     $.ajax({
-      url: ""
+      url: "https://theme-park-268d3.firebaseio.com/areas/.json"
     }).done(function(data) {
     	areaData = data;
       resolve(data);
-    }).fail(function(xhr, status, error) {
+      console.log(data);
+    }).fail(function(error) {
       reject(error);
     });
   });
 };
-
+map.loadAreas();
 map.loadTypes = function() {
   return new Promise((resolve, reject) => {
     $.ajax({
-      url: ""
+      url: "https://theme-park-268d3.firebaseio.com/attraction_types/.json"
     }).done(function(data) {
     	typeData = data;
       resolve(data);
+       console.log(data);
     }).fail(function(xhr, status, error) {
       reject(error);
     });
   });
 };
-
+map.loadTypes();
 map.loadAttractions = function() {
   return new Promise((resolve, reject) => {
     $.ajax({
-      url: ""
+      url: "https://theme-park-268d3.firebaseio.com/attractions/.json"
     }).done(function(data) {
     	attractionData = data;
       resolve(data);
+       console.log(data);
     }).fail(function(xhr, status, error) {
       reject(error);
     });
   });
 };
-
+map.loadAttractions();
 map.loadParkInfo = function() {
   return new Promise((resolve, reject) => {
     $.ajax({
-      url: ""
+      url: "https://theme-park-268d3.firebaseio.com/park-info/.json"
     }).done(function(data) {
     	parkInfo = data;
       resolve(data);
+       console.log(data);
     }).fail(function(xhr, status, error) {
       reject(error);
     });
   });
 };
+map.loadParkInfo();
+// map.getAreaData = () => {
+//   console.log(areaData);
+// 	return areaData;
+// };
 
-map.getAreaData = () => {
-	return areaData;
-};
+// map.getAttractionData = () => {
+// 	return attractionData;
+// };
 
-map.getAttractionData = () => {
-	return attractionData;
-};
-
-map.getTypeData = () => {
-	return typeData;
-};
-map.getParkData = () => {
-	return parkInfo;
-};
+// map.getTypeData = () => {
+// 	return typeData;
+// };
+// map.getParkData = () => {
+// 	return parkInfo;
+// };
 
 
 
