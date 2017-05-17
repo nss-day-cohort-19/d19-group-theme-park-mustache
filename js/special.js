76cd2ccb68578d069fd476e9d5f52a4eb54087b8
Kathleen Weisensel <kweisensel@gmail.com>
2017-05-17 14:11:31 -0500

    starting on timed events

---
+++ js/special.js
@@ -0,0 +1,48 @@
+"use strict";
+
+let _ = require('lodash');
+let requests = require('../js/requests.js');
+let special = {};
+
+requests.loadAttractions()
+.then( (data) => {
+	let correctAreas = special.filterData(data);
+	let timedEvents = special.findTimedEvents(correctAreas);
+});
+
+//filter out the correct areas
+special.filterData = (data) => {
+  var filteredAttractions = _.filter(data, function(item){return item.area_id === 2 ||
+    item.area_id === 3 || item.area_id === 5 || item.area_id === 6 ||
+    item.area_id === 7;});
+  return filteredAttractions;
+};
+
+//filter out the timed events from the correct areas
+//find items with a timed key
+special.findTimedEvents = (data) => {
+	var timedEvents = _.filter(data, "times");
+	console.log("timedEvents", timedEvents);
+	//attempt to pull one item with timed events
+	var firstItemWithTimes = timedEvents[0].times;
+	console.log("firstItemWithTimes", firstItemWithTimes);
+	//sort timed events by type
+	// var allTimes = _.sort(timedEvents)
+};
+
+//get "times" array out of each timedEvents
+
+//parseFloat each time (change string to number value)
+
+//convert military time to regular time to output to DOM
+
+
+
+
+
+
+
+
+
+
+
