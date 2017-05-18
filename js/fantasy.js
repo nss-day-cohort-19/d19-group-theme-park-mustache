d02a3c39c6f62d33698689ed91aca0b808c33930
Willie Pruitt <w_pruitt@hotmail.com>
2017-05-18 11:54:48 -0500

    UGLY COLORS
    I'll need help if you want something more visually appealing Maddie

--- js/fantasy.js
+++ js/fantasy.js
@@ -1,21 +1,24 @@
 'use strict';
 
-let map = require('../js/requests.js');
-let fantasy = {};
-let areaData = [];
-// let attractionData;
-let typeData = [];
+let map = require('../js/requests.js'),
+fantasy = {},
+areaData = [],
+attractionData, typeData = [],
+fantasyTemplate = require('../templates/fantasy-modal.hbs'),
+fantasyData;
+
 
 let Fantasyland = document.getElementById("Fantasyland");
 
 fantasy.popFantasy = function(){
-	let attractionData = map.filterByArea(3);
-	console.log("Mario", attractionData[1]);
-	map.loadAttractions()
-.then((data)=>{
-	fantasy.popFantasyModal(data);
-});
-
+	Fantasyland.innerHTML = "";
+	map.filterByArea(3)
+		.then( (data) =>{
+		attractionData = data;
+		fantasy.popFantasyModal(data);
+		}, (reject) => {
+			console.log(reject);
+	});
 };
 
 fantasy.getFantasyAttractions = () => {
@@ -25,20 +28,22 @@ fantasy.getFantasyAttractions = () => {
 };
 
 fantasy.popFantasyModal = (data) =>{
-	console.log("Rick:", data);
+	fantasyData = data;
 	for (var i = 0; i < data.length; i++){
-		if (data[i].area_id === 3 && data[i].type_id === 1){
-			document.getElementById("31").innerHTML += `<li>${data[i].name}</li>`;
-		}
-		else if (data[i].area_id === 3 && data[i].type_id === 2){
-			document.getElementById("32").innerHTML += `<li>${data[i].name}</li>`;
-		}
-		else if (data[i].area_id === 3 && data[i].type_id === 3){
-			document.getElementById("33").innerHTML += `<li>${data[i].name}</li>`;
-		}
-		else if (data[i].area_id === 3 && data[i].type_id === 4){
-			document.getElementById("34").innerHTML += `<li>${data[i].name}</li>`;
-		}
+		// if (data[i].area_id === 3 && data[i].type_id === 1){
+		// 	document.getElementById("31").innerHTML += `<li>${data[i].name}</li>`;
+		// }
+		// else if (data[i].area_id === 3 && data[i].type_id === 2){
+		// 	document.getElementById("32").innerHTML += `<li>${data[i].name}</li>`;
+		// }
+		// else if (data[i].area_id === 3 && data[i].type_id === 3){
+		// 	document.getElementById("33").innerHTML += `<li>${data[i].name}</li>`;
+		// }
+		// else if (data[i].area_id === 3 && data[i].type_id === 4){
+		// 	document.getElementById("34").innerHTML += `<li>${data[i].name}</li>`;
+		// }
+        console.log(fantasyData[i]);
+        Fantasyland.innerHTML += fantasyTemplate(fantasyData);
 	}
 };
 
