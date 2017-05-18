eb73933d1ca868d6f68d687bad1fb63e9839c2d6
Azim <azimsadikov@gmail.com>
2017-05-18 15:59:17 -0500

    final version

---
+++ js/adventure.js
@@ -0,0 +1,34 @@
+"use strict";
+let requests = require('../js/requests.js');
+let main = require('./main.js');
+let _ = require('lodash');
+
+
+var rides = [];
+var restaurants = [];
+var shows = [];
+var vendors = [];
+
+
+requests.loadAttractions().then((data) => {
+    var adventure = requests.filterData(data);
+    var advRides = _.filter(adventure[0], function(item) {
+        return item.area_id === 2;
+    });
+    var advRidesObj = {Rides : advRides};
+    var advRestaurants = _.filter(adventure[1], function(item) {
+        return item.area_id === 2;
+
+    });
+    var advRestaurantsObj = {Restaurants : advRestaurants};
+    var advShows = _.filter(adventure[2], function(item) {
+        return item.area_id === 2;
+    });
+    var advShowsObj = {Shows : advShows};
+    var advVendors = _.filter(adventure[3], function(item) {
+        return item.area_id === 2;
+    });
+    var advVendorsObj = {Vendors : advVendors};
+    main.populateModal(advRidesObj, advRestaurantsObj, advShowsObj, advVendorsObj);
+});
+
--- js/requests.js
+++ js/requests.js
@@ -91,29 +91,29 @@ map.filterData = (data) => {
 };
 
 
-map.filterByArea = function(areaNum){
-  return new Promise( (resolve, reject) =>{
-    map.loadAttractions()
-      .then( (data) =>{
-        let funArray = map.filterData(data);
-        let newArray = [];
-          // console.log("funArray", funArray);
-        _.forEach(funArray, (value, index) =>{
-          _.forIn(value, (currVal, num) =>{
-            if (currVal.area_id === areaNum){
-              newArray.push(currVal);
-            }
-          });
-        });
-        resolve(newArray);
-        // console.log("filterByArea's array", newArray);
-      }, (error) =>{
-        console.log(error);
-      }
-    );
-    // reject("filter error");
-  });
-};
+// map.filterByArea = function(areaNum){
+//   return new Promise( (resolve, reject) =>{
+//     map.loadAttractions()
+//       .then( (data) =>{
+//         let funArray = map.filterData(data);
+//         let newArray = [];
+//           // console.log("funArray", funArray);
+//         _.forEach(funArray, (value, index) =>{
+//           _.forIn(value, (currVal, num) =>{
+//             if (currVal.area_id === areaNum){
+//               newArray.push(currVal);
+//             }
+//           });
+//         });
+//         resolve(newArray);
+//         // console.log("filterByArea's array", newArray);
+//       }, (error) =>{
+//         console.log(error);
+//       }
+//     );
+//     // reject("filter error");
+//   });
+// };
 
 
 map.getAttractionData = () => {
@@ -127,7 +127,7 @@ map.getParkData = () => {
 	return parkInfo;
 };
 
-console.log(map.filterByArea(3));
+// console.log(map.filterByArea(3));
 
 
 module.exports = map;
